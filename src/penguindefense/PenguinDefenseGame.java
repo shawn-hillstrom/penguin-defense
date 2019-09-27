7fe3af5171998e20c078b008c30dc2ef9b750648
Shawn <pnwshawn@outlook.com>
2019-09-26 19:39:03 -0700

    Added map resources

--- src/penguindefense/PenguinDefenseGame.java
+++ src/penguindefense/PenguinDefenseGame.java
@@ -5,6 +5,8 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import jig.ResourceManager;
+
 /**
  * Penguin Defense is a tile-based, tower defense game. Your objective: to defend your
  * town from ravenous penguins trying to steal your fish.
@@ -27,6 +29,19 @@ public class PenguinDefenseGame extends StateBasedGame {
 	public final int screenHeight;
 	
 	// resources
+	public static final String IMG_BACKGROUND = "penguindefense.resource/background.png";
+	public static final String IMG_TILE_BLANK = "penguindefense.resource/tile-blank.png";
+	public static final String IMG_PATH_STRAIGHT_HOR = "penguindefense.resource/path-straight-horizontal.png";
+	public static final String IMG_PATH_STRAIGHT_VER = "penguindefense.resource/path-straight-vertical.png";
+	public static final String IMG_PATH_TURN_DOWNLEFT = "penguindefense.resource/path-turn-downleft.png";
+	public static final String IMG_PATH_TURN_DOWNRIGHT = "penguindefense.resource/path-turn-downright.png";
+	public static final String IMG_PATH_TURN_UPLEFT = "penguindefense.resource/path-turn-upleft.png";
+	public static final String IMG_PATH_TURN_UPRIGHT = "penguindefense.resource/path-turn-upright.png";
+	public static final String IMG_PATH_3WAY_DOWN = "penguindefense.resource/path-3way-down.png";
+	public static final String IMG_PATH_3WAY_LEFT = "penguindefense.resource/path-3way-left.png";
+	public static final String IMG_PATH_3WAY_RIGHT = "penguindefense.resource/path-3way-right.png";
+	public static final String IMG_PATH_3WAY_UP = "penguindefense.resource/path-3way-up.png";
+	public static final String IMG_PATH_4WAY = "penguindefense.resource/path-4way.png";
 	
 	// entities
 
@@ -48,7 +63,26 @@ public class PenguinDefenseGame extends StateBasedGame {
 
 	@Override
 	public void initStatesList(GameContainer container) throws SlickException {
+		
 		addState(new StartUpState());
+		
+		// initialize images
+		ResourceManager.loadImage(IMG_BACKGROUND);
+		ResourceManager.loadImage(IMG_TILE_BLANK);
+		ResourceManager.loadImage(IMG_PATH_STRAIGHT_HOR);
+		ResourceManager.loadImage(IMG_PATH_STRAIGHT_VER);
+		ResourceManager.loadImage(IMG_PATH_TURN_DOWNLEFT);
+		ResourceManager.loadImage(IMG_PATH_TURN_DOWNRIGHT);
+		ResourceManager.loadImage(IMG_PATH_TURN_UPLEFT);
+		ResourceManager.loadImage(IMG_PATH_TURN_UPRIGHT);
+		ResourceManager.loadImage(IMG_PATH_3WAY_DOWN);
+		ResourceManager.loadImage(IMG_PATH_3WAY_LEFT);
+		ResourceManager.loadImage(IMG_PATH_3WAY_RIGHT);
+		ResourceManager.loadImage(IMG_PATH_3WAY_UP);
+		ResourceManager.loadImage(IMG_PATH_4WAY);
+		
+		// initialize sounds
+		
 	}
 
 	public static void main(String args[]) {
